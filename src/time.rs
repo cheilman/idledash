e6107f92e85d0584d9139717ed7688b120edfdc6
Chris Heilman <chris.heilman@gmail.com>
2025-09-21 21:15:47 -0400

    feat: reorganize panes

--- src/time.rs
+++ src/time.rs
@@ -1,11 +1,19 @@
 use crate::app::AppState;
-use chrono_tz::America::Los_Angeles;
+use chrono_tz::America::{Los_Angeles, New_York};
 use ratatui::{
     prelude::*,
     widgets::{Block, Borders, Paragraph},
 };
 
-pub fn render_time_widgets(f: &mut Frame, rect: Rect, app: &AppState) {
+pub fn render_local_time_widget(f: &mut Frame, rect: Rect, app: &AppState) {
+    let local_time_str = app.local_time.format("%Y-%m-%d\n%H:%M:%S").to_string();
+    let local_time_widget = Paragraph::new(local_time_str)
+        .block(Block::default().title("Local Time").borders(Borders::ALL))
+        .alignment(Alignment::Center);
+    f.render_widget(local_time_widget, rect);
+}
+
+pub fn render_world_time_widget(f: &mut Frame, rect: Rect, app: &AppState) {
     let time_layout = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([
@@ -15,23 +23,28 @@ pub fn render_time_widgets(f: &mut Frame, rect: Rect, app: &AppState) {
         ])
         .split(rect);
 
-    // Local Time
-    let local_time_str = app.local_time.format("%Y-%m-%d\n%H:%M:%S").to_string();
-    let local_time_widget = Paragraph::new(local_time_str)
-        .block(Block::default().title("Local").borders(Borders::ALL))
-        .alignment(Alignment::Center);
-    f.render_widget(local_time_widget, time_layout[0]);
-
     // UTC Time
-    let utc_time_str = app.utc_time.format("%Y-%m-%d\n%H:%M:%S").to_string();
+    let utc_time_str = app.utc_time.format("%H:%M:%S").to_string();
     let utc_time_widget = Paragraph::new(utc_time_str)
         .block(Block::default().title("UTC").borders(Borders::ALL))
         .alignment(Alignment::Center);
-    f.render_widget(utc_time_widget, time_layout[1]);
+    f.render_widget(utc_time_widget, time_layout[0]);
+
+    // East Coast Time
+    let east_coast_time = app.utc_time.with_timezone(&New_York);
+    let east_coast_time_str = east_coast_time.format("%H:%M:%S").to_string();
+    let east_coast_widget = Paragraph::new(east_coast_time_str)
+        .block(
+            Block::default()
+                .title("East Coast (EST)")
+                .borders(Borders::ALL),
+        )
+        .alignment(Alignment::Center);
+    f.render_widget(east_coast_widget, time_layout[1]);
 
     // West Coast Time
     let west_coast_time = app.utc_time.with_timezone(&Los_Angeles);
-    let west_coast_time_str = west_coast_time.format("%Y-%m-%d\n%H:%M:%S").to_string();
+    let west_coast_time_str = west_coast_time.format("%H:%M:%S").to_string();
     let west_coast_widget = Paragraph::new(west_coast_time_str)
         .block(
             Block::default()
--- src/ui.rs
+++ src/ui.rs
@@ -1,34 +1,30 @@
 use crate::app::AppState;
 use crate::disks::render_disk_widgets;
-use crate::system::render_system_widgets;
-use crate::time::render_time_widgets;
+use crate::time::{render_local_time_widget, render_world_time_widget};
 use crate::vcs::render_vcs_widgets;
 use ratatui::prelude::*;
 
 pub fn ui(f: &mut Frame, app: &AppState) {
     let main_layout = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(80), Constraint::Percentage(20)])
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)])
         .split(f.area());
 
     let top_layout = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(70), Constraint::Percentage(30)])
+        .constraints([
+            Constraint::Percentage(20),
+            Constraint::Percentage(40),
+            Constraint::Percentage(40),
+        ])
         .split(main_layout[0]);
 
-    let top_left_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .split(top_layout[0]);
-
-    // System Info
-    render_system_widgets(f, top_left_layout[0], app);
-
     // Time Widgets
-    render_time_widgets(f, top_left_layout[1], app);
+    render_local_time_widget(f, top_layout[0], app);
+    render_world_time_widget(f, top_layout[1], app);
 
     // Disk Usage
-    render_disk_widgets(f, top_layout[1], app);
+    render_disk_widgets(f, top_layout[2], app);
 
     // Version Control
     render_vcs_widgets(f, main_layout[1], app);
